e343d6d5f1fc864219ed1b9ddadb6fad77d283b0
A_robert <alex.braz@academico.ifpb.edu.br>
2026-03-01 15:34:01 -0300

    feat: implementação da funcionalidade de alternância entre modos claro e escuro

---
+++ script.js
@@ -0,0 +1,23 @@
+const schemeSelect = document.getElementById ("dark-mode-toggle");
+const themeIcon = document.getElementById("theme-icon");
+
+// Função para mudar esquema da cor Dark/Light
+schemeSelect.addEventListener("change", function() {
+
+    themeIcon.className = "fas";
+    
+    if (schemeSelect.value == "light") { 
+        document.documentElement.setAttribute("light-dark-mode", "light"); //Claro
+        themeIcon.classList.add("fa-sun");
+    } else if (schemeSelect.value == "dark") {
+        document.documentElement.setAttribute("light-dark-mode", "dark"); //Escuro
+        themeIcon.classList.add("fa-moon");
+    } else {
+        document.documentElement.removeAttribute("light-dark-mode"); //Cor do sistema
+        themeIcon.classList.add("fa-desktop");
+    }
+    //Depurando o modo no console
+    console.log("Modoa alteredo para:" , schemeSelect.value);
+});
+
+
